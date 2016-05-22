2dfa3779b3b7c24462cb55279d4dc80519122880
Eric <eterui42@gmail.com>
2016-05-22 18:00:46 -0400

    fixed bad test 'should allow 3.14 for count'

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -13,16 +13,16 @@ class TagTest < ActiveSupport::TestCase
 
   should validate_numericality_of(:count)
   should allow_value(0).for(:count)
+  should allow_value(2).for(:count)
+
   should_not allow_value(-1).for(:count)
   should_not allow_value(50.50).for(:count)
   should_not allow_value(Date.today).for(:count)
   should_not allow_value(1.day.ago.to_date).for(:count)
   should_not allow_value(1.day.from_now.to_date).for(:count)
   should_not allow_value("bad").for(:count)
-  should allow_value(2).for(:count)
-  should allow_value(3.14159).for(:count)
-  should allow_value(0).for(:count)
-  should allow_value(-3.14159).for(:count)
+  should_not allow_value(3.14159).for(:count)
+  should_not allow_value(-3.14159).for(:count)
   
   def tag
     @tag ||= Tag.new
