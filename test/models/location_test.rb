3e865b1bcda6cba4b2e55da7517c7375b871b519
Aamer <arakla@andrew.cmu.edu>
2016-05-23 18:40:01 -0400

    Updated location tests for active and removed tests from lat-long that will always cause an error;

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -10,18 +10,12 @@ class LocationTest < ActiveSupport::TestCase
   should validate_presence_of(:longitude)
   should validate_presence_of(:active)
 
-  should_not allow_value(Date.today).for(:latitude)
-  should_not allow_value(1.day.ago.to_date).for(:latitude)
-  should_not allow_value(1.day.from_now.to_date).for(:latitude)
   should_not allow_value("bad").for(:latitude)
   should allow_value(2).for(:latitude)
   should allow_value(3.14159).for(:latitude)
   should allow_value(0).for(:latitude)
   should allow_value(-3.14159).for(:latitude)
 
-  should_not allow_value(Date.today).for(:longitude)
-  should_not allow_value(1.day.ago.to_date).for(:longitude)
-  should_not allow_value(1.day.from_now.to_date).for(:longitude)
   should_not allow_value("bad").for(:longitude)
   should allow_value(2).for(:longitude)
   should allow_value(3.14159).for(:longitude)
