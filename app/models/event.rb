7dcee79237a185fa9f12847d0b91a97d7a3bd8dc
Eric <eterui42@gmail.com>
2016-05-23 18:22:46 -0400

    event model relationships, validates_presence_of fields, and chronological, by_location, and for_location scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,22 @@
 class Event < ActiveRecord::Base
+  # Relationships
   belongs_to :location
+  has_many :user_events
+  has_many :event_organizations
+  has_many :event_tags
+  has_many :tags, through: :event_tags
+  has_many :users, through: :user_events
+  has_many :organizations, through: :event_organizations
+
+  # Validations
+  validates_presence_of :name, on: :create
+  validates_presence_of :location, on: :create
+  validates_presence_of :start_time, on: :create
+  validates_presence_of :end_time, on: :create
+
+  # Scopes
+  scope :chronological, -> { order(start_datetime: :desc) }
+  scope :by_location,  -> { order(:location) }
+  scope :for_location,  ->(location_id) { where(location_id: location_id) }
+  
 end
