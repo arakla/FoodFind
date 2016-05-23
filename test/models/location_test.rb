8ebcb989dd792333a569a21c7b847dd2f90c9d3f
Aamer <arakla@andrew.cmu.edu>
2016-05-23 18:05:09 -0400

    Added validations, relationships, and search to location; Added location active tests;

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -8,6 +8,7 @@ class LocationTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
   should validate_presence_of(:latitude)
   should validate_presence_of(:longitude)
+  should validate_presence_of(:active)
 
   should_not allow_value(Date.today).for(:latitude)
   should_not allow_value(1.day.ago.to_date).for(:latitude)
@@ -26,6 +27,15 @@ class LocationTest < ActiveSupport::TestCase
   should allow_value(3.14159).for(:longitude)
   should allow_value(0).for(:longitude)
   should allow_value(-3.14159).for(:longitude)
+
+  should allow_value(FALSE).for(:active)
+  should allow_value(TRUE).for(:active)
+  should_not allow_value(0).for(:active)
+  should_not allow_value(120).for(:active)
+  should_not allow_value("bad").for(:active)
+  should_not allow_value(-20).for(:active)
+  should_not allow_value(3.14159).for(:active)
+  should_not allow_value("1:00").for(:active) 
 
   def location
     @location ||= Location.new
